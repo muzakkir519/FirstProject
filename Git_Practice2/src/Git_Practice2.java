fd8f05286cf85eef413ef9bee03f6d875661f256
muzakkir519 <91741532+muzakkir519@users.noreply.github.com>
2021-10-09 20:34:23 +0530

    Add files via upload

---
+++ Git_Practice2/src/Git_Practice2.java
@@ -0,0 +1,16 @@
+/**
+ * @author Md Muzakkir
+ */
+
+public class Git_Practice2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Sample code");
+		System.out.println("Hello");
+		System.out.println("My Practice");
+	}
+
+}
